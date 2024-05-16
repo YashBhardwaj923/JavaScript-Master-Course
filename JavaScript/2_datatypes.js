7b5cfc835189e023026faed6e0f9f233a7d8ec2b
yashbhardwaj923 <bhardwajyash923@gmail.com>
2024-05-16 15:48:22 +0530

    JS Data Types

--- JavaScript/2_datatypes.js
+++ JavaScript/2_datatypes.js
@@ -18,18 +18,22 @@ The latest ECMAScript standard defines eight data types Out of which seven data 
    5. Null */
 
 //* 1. String :
+/* JavaScript Strings are similar to sentences. They are made up of a list of characters, which is essentially just an “array of characters, like “Hello Yash Tech etc. */
 var str = "Hellow Shree!";
 console.log(str);
 
 //* 2. Boolean :
+/* Represent a logical entity and can have two values: true or false. */
 var bool = true;
 console.log(bool);
 
 //* 3. Number :
+/* JavaScript numbers are always stored in double-precision 64-bit binary format IEEE 754. Unlike other programming languages, you don’t need int, float, etc to declare different numeric values. */
 var num = 34;
 console.log(num);
 
 //* 4. Undefined :
+/* A variable that has not been assigned a value is undefined. */
 var und;
 console.log(und);
 
@@ -37,6 +41,7 @@ var undd = undefined;
 console.log(undd);
 
 //* 5. Null :
+/* This type has only one value that is null. */
 var nl = null;
 console.log(nl);
 
@@ -48,6 +53,11 @@ console.log(nl);
    3. Functions */
 
 //* 1. Object :
+/* - The "object" is a complex data type that allows you to store collections of data.
+   - An object contains properties, defined as a key-value pair.
+   - A property key(name) is always a string but the value can b any data type, like as string, number, boolean or complex data types like array, function and other objects.
+*/
+
 var obj = {
   name: "Shree",
   age: 34,
@@ -56,10 +66,19 @@ var obj = {
 console.log(obj);
 
 //* 2. Array :
+/* - An array is a type of object used for strong multiple values in single variable.
+   - Array is a collection of values.
+   - Each value is separated witha comma(,).
+   - It is writting square breackets[].
+   - The data types of array is object. */
+
 var arr = [10, 20, 30, "Hello"];
 console.log(arr);
 
 //* 3. Function :
+/* - The function is callable object that excuted a block of code.
+   - Since functions are objects, so it's possible to assign them to variables. */
+
 function fun() {
   console.log("Function Body");
 }
