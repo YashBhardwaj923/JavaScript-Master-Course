6fde1a7c035a3d92ca809de5ebabc2f916a6ea36
yashbhardwaj923 <bhardwajyash923@gmail.com>
2024-05-16 15:12:58 +0530

    JS Variables

--- JavaScript/1_variables.js
+++ JavaScript/1_variables.js
@@ -22,6 +22,10 @@ Basic rules to declare a variable in JavaScript:
 
 //? 1. "var" keyword :
 //- Declare a variable in the global scope (accessible anywhere)
+/* "var" is the oldest keyword to declare a variable in javascript.
+   The scope of a "var" is function and global scope.
+   It can be update & re-declared into the scope.
+   It can be declared without initialization. */
 var a;
 console.log(a);
 
@@ -38,6 +42,13 @@ console.log(a);
 
 //? 2. "let" keyword :
 //- Declare a variable in the local scope (only accessible within its block, not outside of it).
+/* "let" keyword is a improve version of the "var" keyword.
+   It introduced in 2015 (ES6).
+   The scope of a "let" is only block scope.
+   It can't accessible outside the particular block like as {}.
+   It can be update but can't re-declared into the scope.
+   It can be declared without initialization. */
+
 let y;
 console.log(y);
 
@@ -49,5 +60,13 @@ console.log(x);
 
 //? 3. "const" keyword :
 //- Declare a constant variable which cannot be reassigned. It is always immutable i.e., once initialized, you cannot change its value.
+/* "const" is a latest version of the javascript.
+   It introduced in 2015 (ES6)
+   The scope of a "const" is also block scope.
+   It can't update & re-declared.
+   It can't be decalred without initialization.
+   IT means user can't be update the const variable once is declared.
+ */
+
 const l = "Constant Data";
 console.log(l);
